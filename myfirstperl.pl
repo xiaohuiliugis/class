5ce56cb3bcda019ae4417f6b941a912cd8789f41
Xiaohui <xiaohuiliugis@gmail.com>
2020-03-03 15:22:08 +0000

    myfirstperl.pl

--- myfirstperl.pl
+++ myfirstperl.pl
@@ -1,13 +1,28 @@
-#!
+#!usr/bin/perl -w
 $DNA='DAFDAFDAFASDF';
 $DNA2 ='DAFDAFDSF';
+#\n new line
+#\t tab
 
 print "here are some DNAs\n\n";
 print $DNA,"\n";
 print $DNA2,"\n\t",
 
+#concatenate DNA
+#''change its content to string,"" retrive its value
 $DNA3 = "$DNA$DNA2";
 #$DNA3 = '$DNA$DNA2';
 print "here are other DNA";
+
+#DNA to RNA
+$DNA = 'XIAOHUILIU';
+
+print "\nHere is the starting DNA:\n";
+print "$DNA\n\n";
+$RNA =$DNA;
+$RNA =~ s/U/T/g;
+print "Here is the result of transcribing RNA:\n";
+print "$RNA\n";
+
 
 
