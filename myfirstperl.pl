9d7d14dd1e182e7a2ce195c14b03f819ea2b5162
Xiaohui <xiaohuiliugis@gmail.com>
2020-03-03 16:11:21 +0000

    access_ncbi.pl

--- myfirstperl.pl
+++ myfirstperl.pl
@@ -24,5 +24,24 @@ $RNA =~ s/U/T/g;
 print "Here is the result of transcribing RNA:\n";
 print "$RNA\n";
 
+#!/usr/local/bin/perl -w
+#library for using perl
+use LWP::Simple;
+#query list of ids
+$gilist ='24475906,224465210,50978625,9507198';
 
+#NCBI url base, the base url to access all other resources
+$base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
 
+#specify the data base variables
+###
+
+###
+#the period is a concatenation of variable and non variable
+$url =$base . "efetch.fcgi?db=nucleotide&id=$gi_list&rettype=acc";
+
+#post the url query to NCBI
+$output=get($url);
+
+#show the output to the screen
+print "$output";
