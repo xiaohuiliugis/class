5ded1b2a57a3464db39fd3890987b434ab6d0470
Xiaohui <xiaohuiliugis@gmail.com>
2020-03-03 15:06:38 +0000

    myfirstperl

--- myfirstperl.pl
+++ myfirstperl.pl
@@ -1,4 +1,13 @@
 #!
-$DNA='DAFDAFDAFASDF'
-print $DNA
+$DNA='DAFDAFDAFASDF';
+$DNA2 ='DAFDAFDSF';
+
+print "here are some DNAs\n\n";
+print $DNA,"\n";
+print $DNA2,"\n\t",
+
+$DNA3 = "$DNA$DNA2";
+#$DNA3 = '$DNA$DNA2';
+print "here are other DNA";
+
 
