74ee85cedbfc3e8c2982abe405587174e75f5fa7
Xiaohui <xiaohuiliugis@gmail.com>
2020-03-03 16:16:41 +0000

    access_ncbi.pl

--- access_ncbi.pl
+++ access_ncbi.pl
@@ -9,10 +9,16 @@ $base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
 
 #specify the data base variables
 ###
-
+#database connection: db=nucleotide
+#specify ids/request: id=$gi_list
+#specify the output to accession number type: rettype=acc
+#if not specified above, return all the result as json on the screen
+# if want the output to be saved elsewhere, use shell command >
 ###
 #the period is a concatenation of variable and non variable
-$url =$base . "efetch.fcgi?db=nucleotide&id=$gi_list";
+$url =$base . "efetch.fcgi?db=nucleotide&id=$gi_list&rettype=acc";
+
+
 
 #post the url query to NCBI
 $output=get($url);
